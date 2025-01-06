a52ccee4356ae30d2556b3af34b51a08eda61bf4
Larissa Mikele <Lary.mikele@gmail.com>
2025-01-06 11:44:29 -0300

    Update 07.questao.py

--- 07.questao.py
+++ 07.questao.py
@@ -1,4 +1,17 @@
-numerador = int(input('Informe um numero : '))
-if numero <= 0:
+numerador = int(input("Informe um número: "))
+if numerador <= 0:
     print("Por favor, insira um número inteiro positivo.")
 else:
+    n = 1
+    soma = 0
+    triangular = False
+    while soma < numerador:
+        soma += n
+        n += 1
+        if soma == numerador:
+            triangular = True
+            break
+    if triangular:
+        print(f"O número {numerador} é triangular.")
+    else:
+        print(f"O número {numerador} não é triangular.")
